c308d7adb0b60156764e1bc7fba38fcff603c311
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:22:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 7</title>
+</head>
+<body>
+<p>
+<?php
+$lastname = "Hill ";
+$firstname = "Jake";
+$age = "21";
+echo "Bonjour ";
+echo $lastname;
+echo $firstname;
+echo " , tu as ";
+echo $age;
+echo " ans";
+ ?>
+</p>
+</body>
+</html>
